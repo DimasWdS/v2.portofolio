60054e6f2d8e2489a29304bb8e030307a83eae8d
DimasWds <dimaswids99@gmail.com>
2025-08-23 23:25:12 +0700

    D

--- javascrip/about/about.js
+++ javascrip/about/about.js
@@ -30,6 +30,68 @@ Object.assign(mainAbout.style, {
     justifyContent: "center",
     overflow: "hidden",
   });
+  // konten
+  {
+    const containerKonten = document.createElement("section");
+    const konten = document.createElement("section");
+    const teksSatu = document.createElement("span");
+    const teksDua = document.createElement("span");
+    const teksTiga = document.createElement("span");
+
+    container.appendChild(containerKonten);
+    containerKonten.appendChild(konten);
+    konten.appendChild(teksSatu);
+    konten.appendChild(teksDua);
+    konten.appendChild(teksTiga);
+
+    teksSatu.textContent = "HELLO THERE 👋";
+    teksDua.textContent = "MY FULL NAME IS";
+    teksTiga.textContent = "DIMAS WIDY SAPUTRA";
+
+    containerKonten.classList.add("container-konten-garuda");
+    konten.classList.add("margin-right-teks-name");
+
+    Object.assign(containerKonten.style, {
+      width: "100%",
+      maxWidth: "80rem",
+      height: "100%",
+      position: "absolute",
+      // backgroundColor: "red",
+      top: "0",
+      zIndex: "2",
+      display: "flex",
+      justifyContent: "end",
+    });
+
+    Object.assign(konten.style, {
+      height: "100%",
+      width: "20rem",
+      // backgroundColor: "green",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      color: "var(--main-font)",
+    });
+    Object.assign(teksSatu.style, {
+      fontSize: "1em",
+      fontWeight: "700",
+    });
+    Object.assign(teksDua.style, {
+      fontWeight: "700",
+      fontSize: "1.5em",
+    });
+    Object.assign(teksTiga.style, {
+      fontSize: "2em",
+      fontWeight: "700",
+    });
+    [teksSatu, teksDua, teksTiga].forEach((el) => {
+      Object.assign(el.style, {
+        // backgroundColor: "var(--footer)",
+        // width: "max-content",
+        padding: "0 1rem",
+      });
+    });
+  }
 
   // background
   {
@@ -79,6 +141,7 @@ Object.assign(mainAbout.style, {
         height: "100%",
         width: "100%",
         // backgroundColor: "cyan",
+        filter: "drop-shadow(4px 6px 6px rgba(0, 0, 0, 0.61))",
       });
     }
 
