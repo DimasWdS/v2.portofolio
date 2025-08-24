9e2c8edf2e98484212f643b07a633a3da1ba4119
DimasWds <dimaswids99@gmail.com>
2025-08-24 23:54:54 +0700

    d

--- javascrip/about/about.js
+++ javascrip/about/about.js
@@ -203,12 +203,61 @@ Object.assign(mainAbout.style, {
     height: "100dvh",
     backgroundColor: "var(--second-background)",
   });
+
   // container tolls scroll
   {
+    function createCardTools(imgIcon, teks) {
+      const container = document.createElement("section");
+      const conImg = document.createElement("div");
+      const img = document.createElement("img");
+      const name = document.createElement("p");
+
+      secondContainer.appendChild(container);
+      container.appendChild(conImg);
+      conImg.appendChild(img);
+      container.appendChild(name);
+
+      name.textContent = teks;
+
+      img.setAttribute("loading", "lazy");
+      img.setAttribute("src", `asset/icon/${imgIcon}`);
+      img.setAttribute("alt", "logo");
+
+      Object.assign(container.style, {
+        padding: "0.4rem",
+        backgroundColor: "var(--line)",
+        margin: "0 1rem 0 0",
+        borderRadius: "10px",
+        display: "flex",
+        alignItems: "center",
+        gap: "0.5rem",
+        flexShrink: "0",
+        boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+      });
+
+      Object.assign(conImg.style, {
+        height: "50px",
+        aspectRatio: "1/1",
+        // backgroundColor: "yellow",
+        borderRadius: "5px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      });
+      Object.assign(name.style, {
+        fontWeight: "800",
+        fontSize: "1.2em",
+        color: "var(--footer)",
+      });
+    }
+
     const containerTech = document.createElement("section");
     const firstContainer = document.createElement("section");
+    const secondContainer = document.createElement("section");
+
     container.appendChild(containerTech);
     containerTech.appendChild(firstContainer);
+    firstContainer.appendChild(secondContainer);
 
     Object.assign(containerTech.style, {
       position: "absolute",
@@ -217,17 +266,40 @@ Object.assign(mainAbout.style, {
       // backgroundColor: "yellow",
       // opacity: "30%",
       top: "0",
+      display: "flex",
       // transform: "translateY(-50%)",
     });
     Object.assign(firstContainer.style, {
+      overflow: "hidden",
       width: "90%",
+      maxWidth: "80rem",
       height: "100%",
-      backgroundColor: "var(--main-font)",
+      backgroundColor: "var(--footer)",
       margin: "auto",
-      borderRadius: "10px",
+      borderRadius: "0 0 10px 10px",
       boeder: "2px solid var(--accen-color)",
-      boxShadow: "rgba(0, 0, 0, 0.69) 0px 3px 8px",
+      // boxShadow: "rgba(0, 0, 0, 0.69) 0px 3px 8px",
+      display: "flex",
     });
+    Object.assign(secondContainer.style, {
+      // width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      // backgroundColor: "yellow",
+      flexShrink: "0",
+      animation: "toolsScroll 20s linear infinite",
+    });
+
+    createCardTools("html.svg", "HTML");
+    createCardTools("css.svg", "CSS");
+    createCardTools("javascript.svg", "JavaScrip");
+    createCardTools("tailwindcss.svg", "Tailwind css");
+
+    for (let i = 0; i < 3; i++) {
+      const conScrollClone = secondContainer.cloneNode(true);
+      firstContainer.appendChild(conScrollClone);
+    }
   }
 }
 
