e76b11cd9c0c68d9b66891ba7ef2ea54a43b9eaf
DimasWds <dimaswids99@gmail.com>
2025-08-31 12:38:30 +0700

    D

--- javascrip/index/home.js
+++ javascrip/index/home.js
@@ -100,6 +100,7 @@ Object.assign(mainHome.style, {
     teks2.textContent = "I'M WIDY";
     teks3.textContent = "EXPERIENCED ILLUSTRATOR AND FRONTEND WEB DEVELOPER";
     konten.textContent = "ILLUSTRATOR | WEB DEVOLOPER";
+    konten.innerHTML = `ILLUSTRATOR <span style="color: var(--accen-color)">|</span> WEB DEVOLOPER`;
 
     Object.assign(conKonten.style, {
       position: "absolute",
